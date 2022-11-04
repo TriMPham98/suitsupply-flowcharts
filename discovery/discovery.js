9aa0fd518ade7a2d97fa7f384273f59cc9cb42c0
Tri Pham <trimpham98@gmail.com>
2022-11-04 15:03:28 -0700

    Link to external .js file

---
+++ discovery/discovery.js
@@ -0,0 +1,22 @@
+console.log("This is a flowchart of the discovery questions for Suit Supply.");
+
+// General
+console.log("What brings you in today?");
+console.log("What's the occasion");
+
+// Work
+console.log("What sector do you work in?");
+console.log("How often do you wear a suit?");
+console.log("How many suits do you own?");
+console.log("How many suits do you own?");
+console.log("What color suits do you already have?");
+
+// Vacation
+console.log("Where are you going on holiday?");
+console.log("What's the weather like there?");
+console.log("What type of trip is it going to be?");
+
+// Wedding/Wedding Guest
+console.log("Is the wedding indoor or outdoor?");
+console.log("Is there a theme or dress code?");
+console.log("What is the look that you're going for?");
