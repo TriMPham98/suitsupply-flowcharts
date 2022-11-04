0bdacbb63ff27d12d32af2da2d01ede70761b28e
Tri Pham <trimpham98@gmail.com>
2022-11-04 13:55:55 -0700

    Add starter discovery questions

--- discovery/discovery.cpp
+++ discovery/discovery.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     cout << "This is a flowchart of the discovery questions for Suit Supply." << endl;
 
-        
+    // General
+    cout << "What brings you in today?" << endl;
+    cout << "What's the occasion?" << endl;
+
+    // Work
+    cout << "What sector do you work in?" << endl;
+    cout << "How often do you wear a suit?" << endl;
+    cout << "How many suits do you own?" << endl;
+    cout << "What color suits do you already have?" << endl;
+
+    // Vacation
+    cout << "Where are you going on holiday?" << endl;
+    cout << "What's the weather like there?" << endl;
+    cout << "What type of trip is it going to be?" << endl;
+
+    // Wedding/Wedding Guest
+    cout << "Is the wedding indoor or outdoor?" << endl;
+    cout << "Is there a theme or dress code?" << endl;
+    cout << "What is the look that you're going for?" << endl;
 
     return 0;
 }
